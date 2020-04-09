0ed4b772c6db9e4dfec3ff4ce7ee56270ec162e6
Mateusz Zawadzki <zwodek01@gmail.com>
2020-04-09 10:19:30 +0200

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+import daftcodeImgUrl from './assets/daftcode.png';
+import './styles/style.sass';
+
+export default function () {
+  const section = document.createElement('section')
+  const img = document.createElement('img')
+  img.src = daftcodeImgUrl
+  section.appendChild(img)
+  document.body.appendChild(section)
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/script.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
